6a7f33e6ea4b545c4e4a1c4f3fceee6f9c5cb6a7
binitrajput <binitrajput5@gmail.com>
2026-09-12 20:50:46 +0530

    Build Chatbot UI layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react"
-let decoder = new TextDecoder
+import "./App.css"
+
 function App(){
   const [question, setQuestion] = useState("")
-  const [answer, setAnswer] = useState("")
+  // const [answer, setAnswer] = useState("")
+  const [messages, setMessages] = useState([])
   async function handleSend(){
-    setAnswer("")
+    setMessages(prevMessages => [
+      ...prevMessages,
+      {
+        "role": "user",
+        "content": question
+      }
+    ])
+    setMessages(prevMessages => [
+      ...prevMessages,
+      {
+        "role": "assistant",
+        "content": ""
+      }
+    ])
     const response = await fetch("http://127.0.0.1:8000/chat", {
       method: "POST",
       headers: {
@@ -14,29 +29,55 @@ function App(){
         "question": question
       })
     })
+    let decoder = new TextDecoder()
     let reader = response.body.getReader()
     let result = await reader.read()
     while(!result.done){
       let text = decoder.decode(result.value)
-      setAnswer(prevAns => prevAns + text)
+      setMessages (prevMessages =>
+        prevMessages.map((message, index)=>{
+        if(index ==prevMessages.length - 1){
+          return {...message, content: message.content + text}
+        }
+        return message
+      })
+      )
+      
       result = await reader.read()
     }
   }
   return(
-    <div>
-      <h1>Hello I am Binit's AI Assistant !</h1>
-      <p>Ask me about Binit</p>
-      <input 
-      type="text" 
-      value = {question}
-      onChange={event=>setQuestion(event.target.value)}
-      placeholder="Ask a question about Binit..."
-      />
-      <button onClick = {handleSend}>
-        send
-      </button>
-      <p>You typed: {question}</p>
-      <h2>{answer}</h2>
+    <div className="app">
+
+      <div className="header">
+        <h1>Hello I am Binit's AI Assistant !</h1>
+        <p>Ask me about Binit</p>
+      </div>
+      
+
+      <div className="chat-area">
+        {messages.map((message, index)=>
+          <div 
+          key={index}
+          className={ message.role ==="user"? "user-message": "assistant-message"}
+          >
+            <p>{message.content}</p>
+          </div>
+        )}
+      </div>
+      
+      <div className="input-area">
+        <input 
+        type="text" 
+        value = {question}
+        onChange={event=>setQuestion(event.target.value)}
+        placeholder="Ask a question about Binit..."
+        />
+        <button onClick = {handleSend}>
+          send
+        </button>
+      </div>
+      
     </div>
   )
 }
